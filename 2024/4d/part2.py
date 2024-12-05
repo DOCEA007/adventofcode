0e8c8791ce87a77d7be81685dca902c2cd85297b
DOCEA007 <docea007@gmail.com>
2024-12-06 00:20:52 +0200

    worked on day 5 part1, still not finished

--- 2024/4d/part2.py
+++ 2024/4d/part2.py
@@ -1,3 +1,4 @@
+# TODO: Finish this
 with open("input.txt", 'r') as file:
     data = [[j for j in i.strip()] for i in file.readlines()]
     vertical = list(zip(*data))
--- 2024/5d/main.py
+++ 2024/5d/main.py
@@ -4,7 +4,20 @@ with open("input.txt", "r") as f:
     data = [i.split() for i in f.read().split("\n\n")]
     [rules[key].append(value) for key, value in (map(int,j.split("|")) for j in data[0])]
     rules = dict(rules)
-    updates = data[1]
-    print(rules)
+    updates = [list(map(int, i.split(","))) for i in data[1]]
+    valids = []
+    for update in updates:
+        print(update)
+        valid = True
+        for index, item in enumerate(update):
+            if item in rules:
+                if not bool(set(update[:index]) & set(rules[item])):
+                    valid = False
+            if not bool(set(update[index-1:]) & set([i for i in rules if item in rules[i]])):
+                valid = False
+        if valid:
+            valids.append(update)
+    print(valids) 
+            
     # key by value in list: list([i for i in rules if search in rules[i]])
     
