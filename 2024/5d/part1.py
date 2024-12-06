d003b01f03b32f598685a3a603f3d9ed7209aa3b
DOCEA007 <docea007@gmail.com>
2024-12-06 17:11:09 +0200

    finished day 5 part 1 and renamed it

--- 2024/5d/part1.py
+++ 2024/5d/part1.py
@@ -1,22 +1,28 @@
 from collections import defaultdict
 with open("input.txt", "r") as f:
-    # rules = defaultdict(list)
     data = [i.split() for i in f.read().split("\n\n")]
-    # [rules[key].append(value) for key, value in (map(int,j.split("|")) for j in data[0])]
-    # rules = dict(rules)
     rules = [list(map(int, i.split("|"))) for i in data[0]]
     updates = [list(map(int, i.split(","))) for i in data[1]]
     valids = []
-    
     for update in updates:
+        valid=True
         for index, item in enumerate(update):
             needed=[x for x in rules if item in x]
             before = update[:index]
             if before!=[]:
-                for last in before:
+                for index2, last in enumerate(before):
                     for i in needed:
-                        if last ==i[0]:
-                            
+                        if last == i[0]:
+                            if index2>index:
+                                valid=False
+                                break
+                        if last == i[1]:
+                            if index2<index:
+                                valid=False
+                                break 
+        if valid:
+            valids.append(update)
+    print(sum([i[len(i)//2] for i in valids]))
 
     
     
