e7ee4cd075d86fcd4279853808d52a68ae2f4ac8
Docea Alexandru <53515603+DOCEA007@users.noreply.github.com>
2024-12-06 08:12:33 +0200

    Update part1.py day 2 to remove debug prints

--- 2024/2d/part1.py
+++ 2024/2d/part1.py
@@ -4,14 +4,11 @@ with open("input.txt", "r") as file:
     for report in data:
         if report == sorted(report) or report == sorted(report)[::-1]:
             correct=True
-            print(report)
             for i in range(1, len(report)):
-                print(report[i])
                 diff = abs(report[i]-report[i-1])
                 if diff == 1 or diff == 2 or diff == 3:
                     continue
                 else:
-                    print(abs(report[i]-report[i-1]), " <-")
                     correct=False
         else:
             correct=False
