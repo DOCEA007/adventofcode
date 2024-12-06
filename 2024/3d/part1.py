c0fbe1fc1b27fe4128bbadbe9a1db1b789ce5611
Docea Alexandru <53515603+DOCEA007@users.noreply.github.com>
2024-12-06 08:37:59 +0200

    Update day 3 part 1 to remove spaces

--- 2024/3d/part1.py
+++ 2024/3d/part1.py
@@ -1,18 +1,14 @@
 import re
-
 def extract_and_multiply(filename):
     total_sum = 0
     pattern = re.compile(r'mul\((\d+),\s*(\d+)\)')
-
     with open(filename, 'r') as file:
         for line in file:
             matches = pattern.findall(line)
             for match in matches:
                 num1, num2 = map(int, match)
                 total_sum += num1 * num2
-
     return total_sum
-
 if __name__ == "__main__":
     filename = 'input.txt'
     result = extract_and_multiply(filename)
