6057e48e577f75809a4245e9b68afd4fddcb3363
DOCEA007 <docea007@gmail.com>
2024-12-08 14:37:41 +0200

    Finished day 4 part 2

--- 2024/4d/part2.py
+++ 2024/4d/part2.py
@@ -1,8 +1,13 @@
 # TODO: Finish this
 with open("input.txt", 'r') as file:
     data = [[j for j in i.strip()] for i in file.readlines()]
-    vertical = list(zip(*data))
-    for i, row in enumerate(data):
-        for j, item in enumerate(row):
-            if j==0 or j==len(row)-1:
-                
+    sum = 0
+    for col_i, column in enumerate(data):
+        for index, item in enumerate(column):
+            if index!=0 and index!=len(column)-1 and col_i!=0 and col_i!=len(data)-1:
+                if item == "A":
+                    dl = data[col_i+1][index-1]+item+data[col_i-1][index+1]
+                    dr = data[col_i+1][index+1]+item+data[col_i-1][index-1]
+                    if (dl == "MAS" or dl == "SAM") and (dr == "MAS" or dr == "SAM"):
+                        sum+=1
+    print(sum)
